d7c295e17b10f99cdd5d48fd72e7ad9213354149
Daniel Longeuay <93006157+dannylongeuay@users.noreply.github.com>
2022-04-12 21:39:48 -0700

    feat: :sparkles: board representation and fen parsing (#2)
    * feat: :sparkles: partial fen parsing
    
    * feat: :sparkles: add side to move and castling rights parsing
    
    * feat: :sparkles: full fen parsing
    
    * feat: :sparkles: preliminary terminal rendering
    
    * feat: :sparkles: add board to string conversion

---
+++ src/board.go
@@ -0,0 +1,340 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Square int
+type Color int
+type File int
+type Rank int
+type Bits uint8
+
+const (
+	Invalid Square = iota
+	WhitePawn
+	WhiteKnight
+	WhiteBishop
+	WhiteRook
+	WhiteQueen
+	WhiteKing
+	BlackPawn
+	BlackKnight
+	BlackBishop
+	BlackRook
+	BlackQueen
+	BlackKing
+	Empty
+)
+
+const (
+	White Color = iota
+	Black
+	Both
+)
+
+const (
+	FILE_A File = iota
+	FILE_B
+	FILE_C
+	FILE_D
+	FILE_E
+	FILE_F
+	FILE_G
+	FILE_H
+	FILE_None
+)
+
+const (
+	RANK_8 Rank = iota
+	RANK_7
+	RANK_6
+	RANK_5
+	RANK_4
+	RANK_3
+	RANK_2
+	RANK_1
+	RANK_None
+)
+
+const BoardSquares int = 120
+
+const StartingFEN string = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
+var SquareIndexes64 = [64]int{
+	21, 22, 23, 24, 25, 26, 27, 28,
+	31, 32, 33, 34, 35, 36, 37, 38,
+	41, 42, 43, 44, 45, 46, 47, 48,
+	51, 52, 53, 54, 55, 56, 57, 58,
+	61, 62, 63, 64, 65, 66, 67, 68,
+	71, 72, 73, 74, 75, 76, 77, 78,
+	81, 82, 83, 84, 85, 86, 87, 88,
+	91, 92, 93, 94, 95, 96, 97, 98,
+}
+
+type board struct {
+	squares      []Square
+	sideToMove   Color
+	fiftyMove    int
+	castleRights Bits
+	epIndex      int
+	halfMove     int
+	fullMove     int
+}
+
+func newBoard(fen string) (*board, error) {
+	fenParts := strings.Split(fen, " ")
+
+	if len(fenParts) != 6 {
+		return nil, fmt.Errorf("FEN parts: %v != 6", len(fenParts))
+	}
+
+	b := board{}
+	b.squares = make([]Square, BoardSquares)
+
+	ranks := strings.Split(fenParts[0], "/")
+	if len(ranks) != 8 {
+		return nil, fmt.Errorf("Ranks: %v != 8", len(ranks))
+	}
+	squareIndex64 := 0
+	for _, rank := range ranks {
+		for _, char := range []rune(rank) {
+			squareIndex := SquareIndexes64[squareIndex64]
+			switch string(char) {
+			case "1":
+			case "2":
+			case "3":
+			case "4":
+			case "5":
+			case "6":
+			case "7":
+			case "8":
+				break
+			case "P":
+				b.squares[squareIndex] = WhitePawn
+				break
+			case "N":
+				b.squares[squareIndex] = WhiteKnight
+				break
+			case "B":
+				b.squares[squareIndex] = WhiteBishop
+				break
+			case "R":
+				b.squares[squareIndex] = WhiteRook
+				break
+			case "Q":
+				b.squares[squareIndex] = WhiteQueen
+				break
+			case "K":
+				b.squares[squareIndex] = WhiteKing
+				break
+			case "p":
+				b.squares[squareIndex] = BlackPawn
+				break
+			case "n":
+				b.squares[squareIndex] = BlackKnight
+				break
+			case "b":
+				b.squares[squareIndex] = BlackBishop
+				break
+			case "r":
+				b.squares[squareIndex] = BlackRook
+				break
+			case "q":
+				b.squares[squareIndex] = BlackQueen
+				break
+			case "k":
+				b.squares[squareIndex] = BlackKing
+				break
+			default:
+				return nil, fmt.Errorf("Invalid piece/digit in fen string: %v", string(char))
+			}
+			count, err := strconv.Atoi(string(char))
+			if err != nil {
+				squareIndex64++
+			} else {
+				for i := 0; i < count; i++ {
+					b.squares[squareIndex] = Empty
+					squareIndex64++
+					squareIndex = SquareIndexes64[squareIndex64]
+				}
+			}
+		}
+	}
+
+	sideToMove := fenParts[1]
+	if sideToMove == "w" {
+		b.sideToMove = White
+	} else if sideToMove == "b" {
+		b.sideToMove = Black
+	} else {
+		return nil, fmt.Errorf("Invalid side to move in fen string: %v", sideToMove)
+	}
+
+	castlingRights := fenParts[2]
+	for _, char := range []rune(castlingRights) {
+		switch string(char) {
+		case "-":
+			break
+		case "K":
+			b.castleRights |= 1 << 3
+			break
+		case "Q":
+			b.castleRights |= 1 << 2
+			break
+		case "k":
+			b.castleRights |= 1 << 1
+			break
+		case "q":
+			b.castleRights |= 1
+			break
+		default:
+			return nil, fmt.Errorf("Invalid castling rights in fen string: %v", string(char))
+		}
+	}
+
+	b.epIndex = -1
+	if fenParts[3] != "-" {
+		var err error
+		b.epIndex, err = squareIndexByCoord(fenParts[3])
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	var err error
+	b.halfMove, err = strconv.Atoi(fenParts[4])
+	if err != nil {
+		return nil, err
+	}
+
+	b.fullMove, err = strconv.Atoi(fenParts[5])
+	if err != nil {
+		return nil, err
+	}
+
+	return &b, nil
+}
+
+func (b board) toString() string {
+	s := ""
+	sep := "\n_________________________\n"
+	s += sep
+	rank := 8
+	for i, index := range SquareIndexes64 {
+		square := b.squares[index]
+		if i != 0 && i%8 == 0 {
+			s += "| "
+			s += strconv.Itoa(rank)
+			rank--
+			s += sep
+		}
+		s += "|"
+		switch square {
+		case Empty:
+			s += " "
+			break
+		case WhitePawn:
+			s += "♟"
+			break
+		case WhiteKnight:
+			s += "♞"
+			break
+		case WhiteBishop:
+			s += "♝"
+			break
+		case WhiteRook:
+			s += "♜"
+			break
+		case WhiteQueen:
+			s += "♛"
+			break
+		case WhiteKing:
+			s += "♚"
+			break
+		case BlackPawn:
+			s += "♙"
+			break
+		case BlackKnight:
+			s += "♘"
+			break
+		case BlackBishop:
+			s += "♗"
+			break
+		case BlackRook:
+			s += "♖"
+			break
+		case BlackQueen:
+			s += "♕"
+			break
+		case BlackKing:
+			s += "♔"
+			break
+		default:
+			s += "♠"
+			break
+		}
+		s += " "
+	}
+	s += "| 1"
+	s += sep
+	s += " A  B  C  D  E  F  G  H"
+	return s
+}
+
+func squareIndexByCoord(s string) (int, error) {
+	var f File
+	var r Rank
+	coordParts := []rune(s)
+	if len(coordParts) != 2 {
+		return -1, fmt.Errorf("Invalid chess notation: %v", s)
+	}
+	switch strings.ToLower(string(coordParts[0])) {
+	case "a":
+		f = FILE_A
+	case "b":
+		f = FILE_B
+	case "c":
+		f = FILE_C
+	case "d":
+		f = FILE_D
+	case "e":
+		f = FILE_E
+	case "f":
+		f = FILE_F
+	case "g":
+		f = FILE_G
+	case "h":
+		f = FILE_H
+	default:
+		return -1, fmt.Errorf("Invalid chess file: %v", string(coordParts[0]))
+	}
+	switch string(coordParts[1]) {
+	case "1":
+		r = RANK_1
+	case "2":
+		r = RANK_2
+	case "3":
+		r = RANK_3
+	case "4":
+		r = RANK_4
+	case "5":
+		r = RANK_5
+	case "6":
+		r = RANK_6
+	case "7":
+		r = RANK_7
+	case "8":
+		r = RANK_8
+	default:
+		return -1, fmt.Errorf("Invalid chess rank: %v", string(coordParts[1]))
+
+	}
+	return squareIndexByFileRank(f, r), nil
+}
+
+func squareIndexByFileRank(f File, r Rank) int {
+	return (21 + int(f)) + int(r)*10
+}
---
+++ src/board_test.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"testing"
+)
+
+type testSquareChecks struct {
+	file   File
+	rank   Rank
+	square Square
+}
+
+func TestNewBoard(t *testing.T) {
+	tests := []struct {
+		fen          string
+		sideToMove   Color
+		castleRights Bits
+		epIndex      int
+		halfMove     int
+		fullMove     int
+		checks       []testSquareChecks
+	}{
+		{StartingFEN, White, 15, -1, 0, 1, []testSquareChecks{
+			{FILE_A, RANK_8, BlackRook},
+			{FILE_B, RANK_8, BlackKnight},
+			{FILE_C, RANK_8, BlackBishop},
+			{FILE_D, RANK_8, BlackQueen},
+			{FILE_E, RANK_8, BlackKing},
+			{FILE_F, RANK_8, BlackBishop},
+			{FILE_G, RANK_8, BlackKnight},
+			{FILE_H, RANK_8, BlackRook},
+			{FILE_A, RANK_7, BlackPawn},
+			{FILE_B, RANK_7, BlackPawn},
+			{FILE_C, RANK_7, BlackPawn},
+			{FILE_D, RANK_7, BlackPawn},
+			{FILE_E, RANK_7, BlackPawn},
+			{FILE_F, RANK_7, BlackPawn},
+			{FILE_G, RANK_7, BlackPawn},
+			{FILE_H, RANK_7, BlackPawn},
+			{FILE_A, RANK_6, Empty},
+			{FILE_H, RANK_3, Empty},
+			{FILE_A, RANK_2, WhitePawn},
+			{FILE_B, RANK_2, WhitePawn},
+			{FILE_C, RANK_2, WhitePawn},
+			{FILE_D, RANK_2, WhitePawn},
+			{FILE_E, RANK_2, WhitePawn},
+			{FILE_F, RANK_2, WhitePawn},
+			{FILE_G, RANK_2, WhitePawn},
+			{FILE_H, RANK_2, WhitePawn},
+			{FILE_A, RANK_1, WhiteRook},
+			{FILE_B, RANK_1, WhiteKnight},
+			{FILE_C, RANK_1, WhiteBishop},
+			{FILE_D, RANK_1, WhiteQueen},
+			{FILE_E, RANK_1, WhiteKing},
+			{FILE_F, RANK_1, WhiteBishop},
+			{FILE_G, RANK_1, WhiteKnight},
+			{FILE_H, RANK_1, WhiteRook},
+			{FILE_None, RANK_None, Invalid},
+		},
+		},
+		{"rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1",
+			Black, 15, 75, 0, 1, []testSquareChecks{
+				{FILE_E, RANK_4, WhitePawn},
+			}},
+		{"rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2",
+			White, 15, 43, 0, 2, []testSquareChecks{
+				{FILE_C, RANK_5, BlackPawn},
+			}},
+		{"rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2",
+			Black, 15, -1, 1, 2, []testSquareChecks{
+				{FILE_F, RANK_3, WhiteKnight},
+			}},
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 1",
+			White, 0, -1, 0, 1, []testSquareChecks{}},
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w q - 0 1",
+			White, 1, -1, 0, 1, []testSquareChecks{}},
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w k - 0 1",
+			White, 2, -1, 0, 1, []testSquareChecks{}},
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w Q - 0 1",
+			White, 4, -1, 0, 1, []testSquareChecks{}},
+		{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w K - 0 1",
+			White, 8, -1, 0, 1, []testSquareChecks{}},
+	}
+	for _, tt := range tests {
+		b, err := newBoard(tt.fen)
+		if err != nil {
+			t.Error(err)
+		}
+		for _, check := range tt.checks {
+			squareIndex := squareIndexByFileRank(check.file, check.rank)
+
+			if b.squares[squareIndex] != check.square {
+				t.Errorf("square: %v != %v", b.squares[squareIndex], check.square)
+			}
+		}
+
+		if b.sideToMove != tt.sideToMove {
+			t.Errorf("side: %v != %v", b.sideToMove, tt.sideToMove)
+		}
+
+		if b.castleRights != tt.castleRights {
+			t.Errorf("castling rights: %v != %v", b.castleRights, tt.castleRights)
+		}
+
+		if b.epIndex != tt.epIndex {
+			t.Errorf("en passant index: %v != %v", b.epIndex, tt.epIndex)
+		}
+
+		if b.halfMove != tt.halfMove {
+			t.Errorf("half move clock: %v != %v", b.halfMove, tt.halfMove)
+		}
+
+		if b.fullMove != tt.fullMove {
+			t.Errorf("full move clock: %v != %v", b.fullMove, tt.fullMove)
+		}
+	}
+}
+
+func TestBoardToString(t *testing.T) {
+	expected := `
+_________________________
+|♖ |♘ |♗ |♕ |♔ |♗ |♘ |♖ | 8
+_________________________
+|♙ |♙ |♙ |♙ |♙ |♙ |♙ |♙ | 7
+_________________________
+|  |  |  |  |  |  |  |  | 6
+_________________________
+|  |  |  |  |  |  |  |  | 5
+_________________________
+|  |  |  |  |  |  |  |  | 4
+_________________________
+|  |  |  |  |  |  |  |  | 3
+_________________________
+|♟ |♟ |♟ |♟ |♟ |♟ |♟ |♟ | 2
+_________________________
+|♜ |♞ |♝ |♛ |♚ |♝ |♞ |♜ | 1
+_________________________
+ A  B  C  D  E  F  G  H`
+
+	board, err := newBoard(StartingFEN)
+	if err != nil {
+		t.Error(err)
+	}
+	if board.toString() != expected {
+		t.Errorf("board to string: %v != %v", board.toString(), expected)
+	}
+}
+
+func TestSquareIndexByFileRank(t *testing.T) {
+	tests := []struct {
+		file     File
+		rank     Rank
+		expected int
+	}{
+		{FILE_A, RANK_8, 21},
+		{FILE_H, RANK_1, 98},
+	}
+	for _, tt := range tests {
+		actual := squareIndexByFileRank(tt.file, tt.rank)
+		if actual != tt.expected {
+			t.Errorf("square: %v != %v", actual, tt.expected)
+		}
+	}
+}
+
+func TestSquareByIndexes64(t *testing.T) {
+	tests := []struct {
+		index    int
+		expected int
+	}{
+		{0, 21},
+		{63, 98},
+	}
+	for _, tt := range tests {
+
+		actual := SquareIndexes64[tt.index]
+		if actual != tt.expected {
+			t.Errorf("square: %v != %v", actual, tt.expected)
+		}
+	}
+}
